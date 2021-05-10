6237bbe8d4aadfa05a32afc00719af95e284a133
mconnaker <mconnaker@gmail.com>
2021-05-10 12:39:06 -0500

    changes to backend

---
+++ backend/index.js
@@ -0,0 +1,7 @@
+module.exports = async function (context, req, data) {
+    context.bindings.outputDocument = data[0];
+    context.bindings.outputDocument.count += 1;
+    context.res = {
+       body: data[0].count
+    };
+  }
