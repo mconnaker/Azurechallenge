2c18e0d6ccdb5b765b4150ef3689633b33eb56e1
mconnaker <mconnaker@gmail.com>
2021-05-10 12:45:17 -0500

    changes to main.js

--- frontend/main.js
+++ frontend/main.js
@@ -2,20 +2,19 @@ window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
 });
 
-const functionApi = 'https://acgarcupdatecount.azurewebsites.net/api/HttpTrigger1?code=AxUYLXmokvsQAAmE7dEN7T87q2w1DcQsKElY1R9jveHDtYHkBghVWQ=='; 
+const apigateway = 'https://acgarcupdatecount.azurewebsites.net/api/HttpTrigger1?code=AxUYLXmokvsQAAmE7dEN7T87q2w1DcQsKElY1R9jveHDtYHkBghVWQ=='; 
 
 const getVisitCount = () => {
     let count = 0;
-    fetch(functionApi, {
+    fetch(apigateway, {
         node: 'cors',
     })
     .then(response => {
         return response.json()
     })
     .then(res => {
-        const count= res.Atributes.visitcount;
-        document.getElementbyId('counter').innerText =count;
-        document.getElementById()'vistorElem').style.display = 'block';   
+        const count= res.Attributes.visitcount;
+        document.getElementbyId('counter').innerText =count; 
     })
     return count;
 }
