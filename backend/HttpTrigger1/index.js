54d7452b02e294a5849e95125c48f74fdf9d2a49
mconnaker <mconnaker@gmail.com>
2021-05-11 20:11:08 -0500

    clearing backend

--- backend/HttpTrigger1/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = async function (context, req, data) {
-  context.bindings.outputDocument = data[0];
-  context.bindings.outputDocument.count += 1;
-  context.res = {
-     body: data[0].count
-  };
-}
